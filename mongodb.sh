e4ef9b6f4ea2c265a855cc540ec5d6fbaa9dd0f8
DELL <surendramolli3@gmail.com>
2023-12-11 12:43:02 +0530

    s

--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,9 @@
-echo -e
+echo -e "\e[36m Downloading mongodb service \e[0m"
+cp /root/robos-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m Installing mongodb service \e[0m"
+yum install mongodb-org -y
+echo -e "\e[36m changing the listen address \e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -e "\e[36m enabling and restarting  mongodb service  \e[0m"
+systemctl enable mongod
+systemctl start mongod
