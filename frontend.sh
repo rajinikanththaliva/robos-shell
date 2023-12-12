4b52ca7940eb26d6721b3590ad3b2d22d388e27e
DELL <surendramolli3@gmail.com>
2023-12-12 11:09:12 +0530

    s

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,11 @@
-echo -e "\e[36m Installing nginx servie \e[0m"
-yum install nginx -y
-echo -e "\e[36m Removing default server content \e[0m"
+echo -e "\e[36m Installing nginx service \e[0m"
+yum install nginx
+echo -e "\e[36m Removing default content \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
-echo -e "\e[36m Downloading custom server content \e[0m"
-curl -O  https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[36m Downloading custom service \e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
-rm -rf frontend.zip
-echo -e "\e[36m Enabling and restarting the Nginx  \e[0m"
+echo -e "\e[36m Enabling and Restarting the nginx service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
-
-
