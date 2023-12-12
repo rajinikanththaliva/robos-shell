f1c031e08ec85aee47757cf97327368e8a46382b
DELL <surendramolli3@gmail.com>
2023-12-12 21:27:27 +0530

    ff

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,12 @@
 echo -e "\e[36m Installing nginx \e[0m"
-yum install nginx -y &>>/tmp/robosho.log
+yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[36m Removing default content \e[0m"
-cd /usr/share/nginx/html  &>>/tmp/robosho.log
+cd /usr/share/nginx/html  &>>/tmp/roboshop.log
 rm -rf *  &>>/tmp/robosho.log
 echo -e "\e[36m Downloading custom server content \e[0m"
-curl -O  /root/robos-sh/frontend.zip   https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/robosho.log
-unzip frontend.zip &>>/tmp/robosho.log
+curl -O  /root/robos-sh/frontend.zip   https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
+unzip frontend.zip &>>/tmp/roboshop.log
  echo -e "\e[36m Enabling and restarting nginx service \e[0m"
- systemctl enable nginx  &>>/tmp/robosho.log
+ systemctl enable nginx  &>>/tmp/roboshop.log
  systemctl restart nginx
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[36m Downloading mongodb repo \e[0m"
-cp /root/robos-shell/mongodb.repo   /etc/yum.repos.d/mongo.repo
+cp /root/robos-shell/mongodb.repo   /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[36m Installing mongodb service \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[36m changing the listen address \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/mongod.conf &>>/tmp/roboshop.log
 echo -e "\e[36m Enabling and restarting mongodb service \e[0m"
-systemctl enable mongod
+systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod
