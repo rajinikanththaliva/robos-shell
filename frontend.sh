4fc1ef0ccfe75b25342ba3f1e7c0c5a8a99e6b6b
DELL <surendramolli3@gmail.com>
2023-12-12 22:31:33 +0530

    ff

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,8 @@ rm -rf *  &>>/tmp/robosho.log
 echo -e "\e[36m Downloading custom server content \e[0m"
 curl -O  /root/robos-sh/frontend.zip   https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 unzip frontend.zip &>>/tmp/roboshop.log
+echo -e "\e[36m configuring reverse proxy \e[0m"
+cp /root/robos-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf
  echo -e "\e[36m Enabling and restarting nginx service \e[0m"
  systemctl enable nginx  &>>/tmp/roboshop.log
  systemctl restart nginx
