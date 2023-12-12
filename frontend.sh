540dbb6345e61c983149817fef2b41bf7fd403cf
DELL <surendramolli3@gmail.com>
2023-12-12 16:07:50 +0530

    er

--- frontend.sh
+++
@@ -1,8 +0,0 @@
-yum install nginx
-rm -rf *
-cd /usr/share/nginx/html
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-unzip frontend.zip
-systemctl enable nginx
-systemctl restart nginx
-
