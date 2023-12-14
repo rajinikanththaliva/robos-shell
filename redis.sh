14cfa397c12c96cfba4a342f53e291a94d4c7f1d
DELL <surendramolli3@gmail.com>
2023-12-14 12:52:21 +0530

    rre

---
+++ redis.sh
@@ -0,0 +1,12 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+Yum module list
+yum module enable redis:remi-6.2 -y
+yum install redis -y
+sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/redis.conf
+systemctl enable redis
+systemctl start redis
+
+
+
+
+
