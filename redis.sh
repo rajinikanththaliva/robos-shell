c04fa3ce2e08967f8cfb0fb430c97273230d09db
DELL <surendramolli3@gmail.com>
2023-12-14 13:26:52 +0530

    rre

--- redis.sh
+++ redis.sh
@@ -3,8 +3,9 @@ Yum module list
 yum module enable redis:remi-6.2 -y
 yum install redis -y
 sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/redis/redis.conf
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
 
 
 
