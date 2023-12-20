27eefd4ab3faeffb4870c5d03f4f920593d461bf
DELL <surendramolli3@gmail.com>
2023-12-20 11:10:10 +0530

    sss

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[36m Downloading mongodb repo \e[0m"
-cp /root/robos-shell/mongodb.repo   /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp /root/robos-shell/mongodb.repo   /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m Installing mongodb service \e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.log
+yum install mongodb-org -y
 echo -e "\e[36m changing the listen address \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/mongod.conf &>>/tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/'   /etc/mongod.conf
 echo -e "\e[36m Enabling and restarting mongodb service \e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
+systemctl enable mongod
 systemctl restart mongod
