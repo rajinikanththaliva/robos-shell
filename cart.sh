044bbda06b4d0cf4e85d8eec8dfe5402a8ed2e86
DELL <surendramolli3@gmail.com>
2023-12-20 14:13:18 +0530

    rrr

--- cart.sh
+++ cart.sh
@@ -9,7 +9,6 @@ cd /app
 echo -e "\e[36m Downloading new app content and their dependencies \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 unzip cart.zip
-rm -rf cart.zip
 npm install
 echo -e "\e[36m Creating cart service file \e[0m"
 cp /root/robos-shell/cart.service    /etc/systemd/system/cart.service
